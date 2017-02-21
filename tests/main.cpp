2f386f981eb2740bb0b0b58781f52aa358a42342
kalimdior <kalimdior@mail.ru>
2017-02-21 14:12:37 +0300

    tests add

---
+++ tests/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * \file main.cpp
+ * \brief 
+ * \author Andrew Bogdanov (AB), andreyb@dins.ru
+ * \copyright Copyright (c) 1999-2017 RingCentral, Inc.
+ *
+ * CHANGE HISTORY
+ * 
+ * \date 
+ * - 21.02.2017 created by AB(Andrew Bogdanov)
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    cout << "Hello, world!" << endl;
+            return 0;
+}
