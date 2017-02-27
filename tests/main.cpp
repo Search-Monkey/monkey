7ab3adc52d7f8d3e7002e452b72323402518c9a8
Ivan Murashko <ivan.murashko@gmail.com>
2017-02-27 14:29:03 +0300

    tests for mul included

--- tests/main.cpp
+++ tests/main.cpp
@@ -15,12 +15,3 @@
 
 #include <iostream>
 #include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(helltest)
-
-BOOST_AUTO_TEST_CASE(test1)
-{
-   BOOST_CHECK_EQUAL(1, 2);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
